1f82297289943aa4442a8ca1ecd1b8e973a73365
ticavin <36242847+ticavin@users.noreply.github.com>
2022-05-30 02:45:13 +0300

    Update reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,8 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	for _, v := range input {
+        output = string(v) + output
+    }
+    return
 }
