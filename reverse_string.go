d9d77f03a2890f6c6abe4b3230f5500cfaace33a
ticavin <606740@gmail.com>
2022-05-30 02:54:42 +0300

    init

--- reverse_string.go
+++ reverse_string.go
@@ -2,7 +2,7 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	for _, v := range input {
-        output = string(v) + output
-    }
-    return
+		output = string(v) + output
+	}
+	return output
 }
